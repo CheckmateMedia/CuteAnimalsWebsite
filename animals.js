45c32086d0675f53172c7d52bcff32e387e555af
Checkmate Media <34839284+CheckmateMedia@users.noreply.github.com>
2020-08-22 11:39:50 -0600

    Add files via upload

--- animals.js
+++ animals.js
@@ -1,3 +1,28 @@
-function close() {
-    document.getElementById('close').style.display = "none"
+function abert() {
+    document.getElementById("abert").style.marginLeft = '20%'
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("abert").innerHTML = 'Favorites: ' + localStorage.clickcount;
+        // document.getElementById("abert").disabled = true;
+    } else {
+        document.getElementById("abert").innerHTML = "Sorry, your browser have favorite animals. Lame.";
+    }
+}
+function dolphin() {
+    document.getElementById("BabyDolphin").style.marginLeft = '20%'
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("BabyDolphin").innerHTML = 'Favorites: ' + localStorage.clickcount;
+        // document.getElementById("abert").disabled = true;
+    } else {
+        document.getElementById("BabyDolphin").innerHTML = "Sorry, your browser have favorite animals. Lame.";
+    }
 }
